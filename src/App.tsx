52694c888a35855d1b781ca1da19bb8950b3221b
AndDevCarv <andredeco2003@gmail.com>
2025-11-02 23:09:08 -0300

    ajustes no css e montagem do heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import "./styles/global.css";
+import "./styles/theme.css";
+
+import { Heading } from "./components/heading";
+import { Timer } from "lucide-react";
+
 export function App() {
   return (
     <>
-      <h1>teste</h1>
-      <p>paragrafo</p>
+      <Heading>
+        Olá mundo
+        <button>
+          <Timer/>
+        </button>
+        </Heading>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio at adipisci tempore accusantium quidem atque, rerum soluta minus accusamus ea vero voluptatem optio quam tenetur, id deleniti. Necessitatibus, perspiciatis nisi.</p>
     </>
   );
 }
