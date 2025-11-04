e9d5ce1bb8d0327484de629cd47eb6ed0d4dd33e
andre <andredeco2003@gmail.com>
2025-11-04 17:54:10 -0300

    adição de novo componente

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import "./styles/theme.css";
 
 import {Container} from "./components/Container";
 import {Heading} from "./components/Heading";
+import { Logo } from "./components/Logo";
 
 export function App() {
   return (
     <>
+      <Container>
+        <Logo>Logo</Logo>
+      </Container>
       <Container>
         <Heading>Form</Heading>
       </Container>
---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,10 @@
+import type React from "react";
+import styles from "./styles.module.css";
+
+type LogoProps = {
+  children: React.ReactNode;
+};
+
+export function Logo({ children }: LogoProps) {
+  return <div className={styles.logo}>{children}</div>;
+}
