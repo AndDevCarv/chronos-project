524a3606b370e3d6e848b77076532f0689485f4a
AndDevCarv <andredeco2003@gmail.com>
2025-11-03 22:30:46 -0300

    criacao de novos componentes e ajustes de pastas

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import "./styles/global.css";
 import "./styles/theme.css";
 
-import { Heading } from "./components/heading";
-import { Timer } from "lucide-react";
+import {Container} from "./components/Container";
+import {Heading} from "./components/Heading";
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá mundo
-        <button>
-          <Timer/>
-        </button>
-        </Heading>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio at adipisci tempore accusantium quidem atque, rerum soluta minus accusamus ea vero voluptatem optio quam tenetur, id deleniti. Necessitatibus, perspiciatis nisi.</p>
+      <Container>
+        <Heading>Form</Heading>
+      </Container>
     </>
   );
 }
