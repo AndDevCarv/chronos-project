fe0bba90114f207665be6c4abbe0bd21d22040b9
AndDevCarv <andredeco2003@gmail.com>
2025-11-06 22:50:08 -0300

    estilizacao do input

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import { CountDown } from "./components/CountDown";
+import { DefaultInput } from "./components/DefaultInput";
 
 export function App() {
   return (
@@ -20,9 +21,8 @@ export function App() {
       </Container>
       <Container>
         <form className="form">
-          <div className="formRow"> 
-            <label htmlFor="meuInput">Task</label>
-            <input id="meuInput"></input>
+          <div className="formRow">
+            <DefaultInput id="meuInput" type="string" labelText="Task" placeholder="Digite algo" />
           </div>
           <div className="formRow">
             <p>Lorem ipsum dolor sit amet.</p>
---
+++ src/components/DefaultInput/index.tsx
@@ -0,0 +1,21 @@
+import styles from "./styles.module.css";
+import type React from "react";
+
+type DefaultInputProps = {
+  id: string;
+  labelText?: string;
+} & React.ComponentProps<"input">;
+
+export function DefaultInput({
+  type,
+  id,
+  labelText,
+  ...rest
+}: DefaultInputProps) {
+  return (
+    <>
+      {labelText && <label htmlFor={id}>{labelText}</label>}
+      <input className={styles.input} id={id} type={type} {...rest}></input>
+    </>
+  );
+}
