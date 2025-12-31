0df288996795d9ea8d4ed85434af5bac337bd2fa
André Carvalho <andredeco2003@gmail.com>
2025-12-31 17:02:06 -0300

    ajustes gerais

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,4 +1,4 @@
-import { PlayCircleIcon } from "lucide-react";
+import { PlayCircleIcon, StopCircleIcon } from "lucide-react";
 import { Cycle } from "../Cycles/Cycle";
 import { DefaultButton } from "../DeafultButton";
 import { DefaultInput } from "../DefaultInput";
@@ -7,21 +7,21 @@ import type { TaskModel } from "../../../models/TaskModel";
 import { useTaskContext } from "../../Contexts/TaskContext/UseTaskContext";
 import { getNextCycle } from "../../../Util/getNextCycle";
 import { getNextCycleType } from "../../../Util/getNextCycleType";
-import {formatedSecondsToMinutes} from "../../../Util/formatedSecondsToMinutes"
+import { formatedSecondsToMinutes } from "../../../Util/formatedSecondsToMinutes";
 
 export function MainForm() {
-  const taskInput = useRef<HTMLInputElement>(null);
+  const taskInput = useRef<HTMLInputElement>(null); //o useRef é muito usado para acessar diretamente elementos do DOM ou guardar valores que não precisam disparar atualização de tela. Diferente do useState que sempre fica atualizando a cada render.
   const { state, setState } = useTaskContext();
 
   const nextCycle = getNextCycle(state.currentCycle);
   console.log(nextCycle);
   const nextCycleType = getNextCycleType(nextCycle);
 
-  function HandleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     if (taskInput.current === null) return;
 
-    const taskName = taskInput.current.value.trim();
+    const taskName = taskInput.current.value.trim(); //taskInput.current é o próprio input no DOM, permitindo acessar .value
 
     if (!taskName) {
       alert("Digite o nome da tarefa.");
@@ -45,33 +45,73 @@ export function MainForm() {
         ...prevState,
         activeTask: newTask,
         currentCycle: nextCycle,
-        secondsRemaining, 
+        secondsRemaining,
         formatedSecondsRemaining: formatedSecondsToMinutes(secondsRemaining),
-        tasks: [...prevState.tasks, newTask], //conferir
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
+
+  function handlerInterruptTask() {
+    setState((prevstate) => {
+      return {
+        ...prevstate,
+        activeTask: null,
+        currentCycle: 0,
+        secondsRemaining: 0,
+        formatedSecondsRemaining: "00:00",
+        tasks: prevstate.tasks.map((task) => {
+          if (prevstate.activeTask && prevstate.activeTask.id == task.id) {
+            return { ...task, interruptDate: Date.now() };
+          }
+          return task;
+        }),
       };
     });
   }
 
   return (
     <>
-      <form onSubmit={HandleCreateNewTask} className="form">
+      <form onSubmit={handleCreateNewTask} className="form">
         <div className="formRow">
           <DefaultInput
             id="meuInput"
             type="string"
             labelText="Task"
             placeholder="Digite algo"
-            ref={taskInput}
+            ref={taskInput} //conectamos o useRef ao DOM
+            disabled={!!state.activeTask} //o !! força o resultado ser true ou false, se for null vai ser convertido para false, se for um obj vai ser convertido para true
           />
         </div>
         <div className="formRow">
           <p>O próximo descanso é em 25min</p>
         </div>
+
+        {state.currentCycle > 0 && ( //Se state.currentCycle > 0, renderiza a <div> com <Cycle />. Caso contrário, não renderiza nada
+          <div className="formRow">
+            <Cycle />
+          </div>
+        )}
         <div className="formRow">
-          <Cycle />
-        </div>
-        <div className="formRow">
-          <DefaultButton icon={<PlayCircleIcon />} />
+          {!state.activeTask ? (
+            <DefaultButton
+              aria-label="Iniciar nova tarefa"
+              title="Iniciar nova tarefa"
+              type="submit"
+              key={"submit"} //key para o react não reaproveitar o botão e causar bug no submit
+              icon={<PlayCircleIcon />}
+            />
+          ) : (
+            <DefaultButton
+              aria-label="Interromper tarefa"
+              title="Interromper tarefa"
+              type="button"
+              color="red"
+              key={"button"} //key para o react não reaproveitar o botão e causar bug no submit
+              onClick={handlerInterruptTask}
+              icon={<StopCircleIcon />}
+            />
+          )}
         </div>
       </form>
     </>
