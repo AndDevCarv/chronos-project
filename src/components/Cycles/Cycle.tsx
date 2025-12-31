016924a461cfde8f80a711ba63bdc27e4e2dccc1
André Carvalho <andredeco2003@gmail.com>
2025-12-30 22:46:47 -0300

    ajustes dos ciclos

--- src/components/Cycles/Cycle.tsx
+++ src/components/Cycles/Cycle.tsx
@@ -1,18 +1,35 @@
+import { getNextCycle } from "../../../Util/getNextCycle";
+import { getNextCycleType } from "../../../Util/getNextCycleType";
+import { useTaskContext } from "../../Contexts/TaskContext/UseTaskContext";
 import styles from "./styles.module.css";
 
 export function Cycle() {
+  const { state } = useTaskContext();
+
+  const cycleStep = Array.from({ length: state.currentCycle }); //vai fazer com que a cada cycle crie um indice no array, iniciamos com um array vazio e ele aumenta junto com os cycles.
+
+  const cycleMap = {
+    workTime: "foco",
+    shortBreak: "descanso curto",
+    longBreak: "descanso longo",
+  };
+
   return (
     <div className={styles.cycleContainer}>
       <span>Ciclos</span>
       <div className={styles.dotsContainer}>
-        <span className={`${styles.dot} ${styles.work}`}></span>
-        <span className={`${styles.dot} ${styles.shortBreak}`}></span>
-        <span className={`${styles.dot} ${styles.work}`}></span>
-        <span className={`${styles.dot} ${styles.shortBreak}`}></span>
-        <span className={`${styles.dot} ${styles.work}`}></span>
-        <span className={`${styles.dot} ${styles.shortBreak}`}></span>
-        <span className={`${styles.dot} ${styles.work}`}></span>
-        <span className={`${styles.dot} ${styles.longBreak}`}></span>
+        {cycleStep.map((_, index) => {
+          const nextCycle = getNextCycle(index);
+          const nextCycleType = getNextCycleType(nextCycle);
+          return (
+            <span
+              key={nextCycle}
+              className={`${styles.dot} ${styles[nextCycleType]}`}
+              aria-label={`Indicador de ciclo de ${cycleMap[nextCycleType]}`}
+              title={`Indicador de ciclo de ${cycleMap[nextCycleType]}`}
+            ></span>
+          );
+        })}
       </div>
     </div>
   );
