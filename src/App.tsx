9bdc95e884cf9a5769a1074e5dfaa9022f652531
André Carvalho <andredeco2003@gmail.com>
2026-01-09 23:13:42 -0300

    exercicio reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Home } from "../pages/Home";
 import { TextContextProvider } from "./Contexts/TaskContext/TaskProvider";
 
+import { ToDoApp } from "../ambiente de testes/reducer";
+
 import "./styles/global.css";
 import "./styles/theme.css";
 
 export function App() {
   return (
-    <TextContextProvider>
-      <Home />
-    </TextContextProvider>
+    // <TextContextProvider>
+    //   <Home />
+    // </TextContextProvider>
+    <ToDoApp />
   );
 }
